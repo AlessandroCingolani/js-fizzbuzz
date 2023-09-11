b98d2011401beeee521eac5c54029982104ae1ac
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-11 13:54:13 +0200

    [add]:console log for check modules

--- js/script.js
+++ js/script.js
@@ -5,11 +5,9 @@
 
 for (let i = 1; i <= 100 ; i++){
   if(!(i % 3)) {
-    // console.log('Fizz');
+    console.log(i + ' Fizz');
   }else if(!(i % 5)){
-    // console.log('Buzz');
-  }else if((i % 3) && (i % 5)){
-    console.log('FizzBuzz');
+    console.log(i + ' Buzz');
   }
     
 
