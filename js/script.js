e46f613de7e90599b14f4a8be41b0964c8513984
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-11 14:51:14 +0200

    [add]:box print in DOM

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 /*
 1. aggiungo loop for per prendere tutti i numeri da 1 a 100 
-2. faccio un controllo se il resto di 3 è zero mi scrive Fizz
+2. faccio un controllo per fizzbuzz fizz e buzz con il modulo
+3. creo const del contenitore box per mettere dentro i box generati
 */
 
+const containerBox = document.querySelector('.container-box')
+
 for (let i = 1; i <= 100 ; i++){
+
+  const box = document.createElement('div');
+  box.classList.add('box');
+  box.append(i);
+  containerBox.append(box)
   if(!(i % 3) && !(i % 5)) {
     console.log(i + ' FizzBuzz');
-   }else if(!(i % 3)){
+  }else if(!(i % 3)){
     console.log(i + ' Fizz');
   }else if (!(i % 5)){
     console.log(i + ' Buzz');
