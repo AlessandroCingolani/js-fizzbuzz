9031b0ad194ad73e104d335a6410bd23326555ce
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-11 14:00:02 +0200

    [fix]:order for if

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@
 */
 
 for (let i = 1; i <= 100 ; i++){
-  if(!(i % 3)) {
+  if(!(i % 3) && !(i % 5)) {
+    console.log(i + ' FizzBuzz');
+   }else if(!(i % 3)){
     console.log(i + ' Fizz');
-  }else if(!(i % 5)){
+  }else if (!(i % 5)){
     console.log(i + ' Buzz');
   }
     
